c66d3b5b6b0e8a3f1afc86591c826b6f58cc19d6
HMuraja <hilla.muraja@hotmail.com>
2022-12-19 15:54:58 +0000

    Add subtract and muliply calculations

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         });
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
     runGame("addition");
 });
 
@@ -22,12 +27,20 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 function runGame(gameType){
+    document.getElementById("answer-box").value="";
+    document.getElementById("answer-box").focus();
     //Creates two random numbers between 1 and 25
     let num1=Math.floor(Math.random()*25)+1;
     let num2=Math.floor(Math.random()*25)+1;
 
     if (gameType === "addition"){
         displayAdditionQuestion (num1, num2);
+    } else if (gameType === "subtract") {
+        largerNum= Math.max(num1,num2);
+        smallerNum=Math.min(num1,num2);        
+        displaySubtractQuestion (largerNum, smallerNum);        
+    } else if(gameType === "multiply"){
+        displayMultiplyQuestion (num1, num2);
     } else {
         alert(`Unknown game type: $(gameType)`);
         throw `Unknown game type: $(gameType). Aborting!`;
@@ -67,6 +80,10 @@ function CalculateCorrectAnswer(){
 
     if (operator==="+"){
         return [operand1 + operand2, "addition"];
+    }else if (operator==="-"){
+        return [operand1 - operand2, "subtract"];
+    }else if (operator==="x"){
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -95,5 +112,13 @@ function displayAdditionQuestion(operand1, operand2){
     document.getElementById('operator').textContent = "+";
 
 }
-function displaySubtractQuestion(){}
-function displayMultiplyQuestion(){}
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+}
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+}
